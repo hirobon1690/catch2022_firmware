dcdc0cc699fcdb38028d5ecd0e2d9f70985f3172
hirobon1690 <hirobon1690@gmail.com>
2022-05-30 13:20:10 +0900

    Lチカ

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,28 @@
 #include <Arduino.h>
+#include <Wire.h>
 
 void setup() {
-  Serial.begin(9600);
-  // put your setup code here, to run once:
+    Wire.begin();
+    Serial.begin(9600);
+    Wire.beginTransmission(0x20);
+    Wire.write(0b00000110);
+    Wire.write(0b00000000);
+    Wire.write(0b00000000);
+    Wire.endTransmission();
+    while (!Serial);  // Leonardo: wait for serial monitor
 }
 
 void loop() {
-  printf("hello\n");
-  delay(100);
+    Wire.beginTransmission(0x20);
+    Wire.write(0b00000010);
+    Wire.write(0b11111111);
+    Wire.write(0b11111111);
+    Wire.endTransmission();
+    delay(500);  // wait 5 seconds for next scan
+    Wire.beginTransmission(0x20);
+    Wire.write(0b00000010);
+    Wire.write(0b00000000);
+    Wire.write(0b00000000);
+    Wire.endTransmission();
+    delay(500);
 }
