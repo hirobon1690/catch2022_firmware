43fedfe81cac57d36dd2335707a97374c3d506c9
hirobon1690 <hirobon1690@gmail.com>
2022-06-16 02:08:46 +0900

    test: MD出力

--- src/main.cpp
+++ src/main.cpp
@@ -2,18 +2,19 @@
 #include <driver/i2c.h>
 #include <esp32/rom/ets_sys.h>
 #include <stdio.h>
+#include "PinNames.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-#include "i2c.h"
-#include "PinNames.h"
 #include "gpio.h"
 #include "gpioex.h"
+#include "i2c.h"
 
-gpio LED(E00,INPUT_PU);
-gpio LED2(E11,OUTPUT);
+gpio pwm1(P25,OUTPUT);
+gpio dir0(E12,OUTPUT);
+gpio dir1(E11,OUTPUT);
 
 extern "C" {
-void app_main(void);
+    void app_main(void);
 }
 
 void delay_ms(int ms) {
@@ -21,16 +22,16 @@ void delay_ms(int ms) {
 }
 
 void app_main() {
+    delay_ms(10);
     i2c.init();
     ex.set();
-    // zero-initialize the config structure.
-    // bool level = 0;
-    // LED=1;
-    LED2=1;
+    pwm1.write(0);
+    dir0.write(0);
+    dir1.write(1);
     while (1) {
-        // LED.flip();
-        LED2.flip();
-        printf("%d\n",bool(LED));
-        delay_ms(500);
+        pwm1.write(1);
+        delay_ms(1);
+        // pwm1.write(0);
+        // delay_ms(1);
     }
 }
