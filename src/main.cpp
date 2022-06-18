6a65fd6596271fae5cc28bce18dfa700caea7676
hirobon1690 <hirobon1690@gmail.com>
2022-06-18 18:33:43 +0900

    TEST: ADC

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include <driver/adc.h>
 #include <driver/gpio.h>
 #include <driver/i2c.h>
 #include <driver/mcpwm.h>
@@ -22,49 +23,18 @@ void delay_ms(int ms) {
     ets_delay_us(ms * 1000);
 }
 
+
+
 void app_main() {
     delay_ms(10);
     i2c.init();
     ex.set();
-    dir0.write(1);
-    dir1.write(0);
-    mcpwm_gpio_init(MCPWM_UNIT_0, MCPWM0A, (int)P25);
-    mcpwm_config_t pwmconfig;
-    pwmconfig.frequency = 100000;
-    pwmconfig.cmpr_a = 0;  // duty cycle of PWMxA = 0
-    pwmconfig.counter_mode = MCPWM_UP_COUNTER;
-    pwmconfig.duty_mode = MCPWM_DUTY_MODE_0;
-    mcpwm_init(MCPWM_UNIT_0, MCPWM_TIMER_0, &pwmconfig);
-    mcpwm_set_frequency(MCPWM_UNIT_0, MCPWM_TIMER_0, 10000);
-    printf("init\n");
+    adc2_config_channel_atten(ADC2_CHANNEL_2, ADC_ATTEN_DB_11);
+    int result = 0;
+
     while (1) {
-        dir0.write(1);
-        dir1.write(0);
-        for (float i = 0; i < 100; i += 0.01) {
-            mcpwm_set_duty(MCPWM_UNIT_0, MCPWM_TIMER_0, MCPWM_OPR_A, i);
-            // delay_ms(10);
-            vTaskDelay(1 / portTICK_RATE_MS);
-            printf("%f\n", i);
-        }
-        for (float i = 100; i > 0; i -= 0.01) {
-            mcpwm_set_duty(MCPWM_UNIT_0, MCPWM_TIMER_0, MCPWM_OPR_A, i);
-            // delay_ms(10);
-            vTaskDelay(1 / portTICK_RATE_MS);
-            printf("%f\n", i);
-        }
-        dir0.write(0);
-        dir1.write(1);
-        for (float i = 0; i < 100; i += 0.01) {
-            mcpwm_set_duty(MCPWM_UNIT_0, MCPWM_TIMER_0, MCPWM_OPR_A, i);
-            // delay_ms(10);
-            vTaskDelay(1 / portTICK_RATE_MS);
-            printf("%f\n", i);
-        }
-        for (float i = 100; i > 0; i -= 0.01) {
-            mcpwm_set_duty(MCPWM_UNIT_0, MCPWM_TIMER_0, MCPWM_OPR_A, i);
-            // delay_ms(10);
-            vTaskDelay(1 / portTICK_RATE_MS);
-            printf("%f\n", i);
-        }
+        adc2_get_raw(ADC2_CHANNEL_2, ADC_WIDTH_BIT_12, &result);
+        printf("%d\n", result);
+        vTaskDelay(1);
     }
 }
