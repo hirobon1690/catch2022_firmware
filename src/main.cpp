330de7238542a3f16da2385784735c3603acfdd2
hirobon1690 <hirobon1690@gmail.com>
2022-06-18 13:48:19 +0900

    TEST: mcpwmに成功

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include <driver/gpio.h>
 #include <driver/i2c.h>
+#include <driver/mcpwm.h>
 #include <esp32/rom/ets_sys.h>
+#include <rom/ets_sys.h>
 #include <stdio.h>
 #include "PinNames.h"
 #include "freertos/FreeRTOS.h"
@@ -9,29 +11,43 @@
 #include "gpioex.h"
 #include "i2c.h"
 
-gpio pwm1(P25,OUTPUT);
-gpio dir0(E12,OUTPUT);
-gpio dir1(E11,OUTPUT);
-
 extern "C" {
-    void app_main(void);
+void app_main(void);
 }
 
+gpio dir0(E12, OUTPUT);
+gpio dir1(E11, OUTPUT);
+
 void delay_ms(int ms) {
-    vTaskDelay((TickType_t)ms / portTICK_RATE_MS);
+    ets_delay_us(ms * 1000);
 }
 
 void app_main() {
     delay_ms(10);
     i2c.init();
     ex.set();
-    pwm1.write(0);
-    dir0.write(0);
-    dir1.write(1);
+    dir0.write(1);
+    dir1.write(0);
+    mcpwm_gpio_init(MCPWM_UNIT_0, MCPWM0A, (int)P25);
+    mcpwm_config_t pwmconfig;
+    pwmconfig.frequency = 10000;
+    pwmconfig.cmpr_a = 0;  // duty cycle of PWMxA = 0
+    pwmconfig.counter_mode = MCPWM_UP_COUNTER;
+    pwmconfig.duty_mode = MCPWM_DUTY_MODE_0;
+    mcpwm_init(MCPWM_UNIT_0, MCPWM_TIMER_0, &pwmconfig);
+    printf("init\n");
     while (1) {
-        pwm1.write(1);
-        delay_ms(1);
-        pwm1.write(0);
-        delay_ms(1);
+        for (float i=0; i < 100; i += 0.5) {
+            mcpwm_set_duty(MCPWM_UNIT_0, MCPWM_TIMER_0, MCPWM_OPR_A, i);
+            delay_ms(500);
+            vTaskDelay(1);
+            printf("%f\n",i);
+        }
+        for (float i=100; i > 0; i -= 0.5) {
+            mcpwm_set_duty(MCPWM_UNIT_0, MCPWM_TIMER_0, MCPWM_OPR_A, i);
+            delay_ms(500);
+            vTaskDelay(1);
+            printf("%f\n",i);
+        }
     }
 }
