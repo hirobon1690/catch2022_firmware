456d8f722030be9ae3235540ec9e7b8c4e4ce4e2
hirobon1690 <hirobon1690@gmail.com>
2022-08-04 16:56:08 +0900

    UPDATE: Stepper

--- src/main.cpp
+++ src/main.cpp
@@ -16,11 +16,12 @@ extern "C" {
 void app_main(void);
 }
 
-gpio dir0(E04, OUTPUT);
-gpio dir1(E01, OUTPUT);
+gpio dir(E05, OUTPUT);
+gpio slp(E04, OUTPUT);
+gpio stp(P18,OUTPUT);
 
 void delay_ms(int ms) {
-    ets_delay_us(ms * 1000);
+    vTaskDelay(ms/portTICK_PERIOD_MS);
 }
 
 
@@ -29,20 +30,11 @@ void app_main() {
     delay_ms(10);
     i2c.init();
     ex.set();
-    dir0.write(1);
-    dir1.write(0);
-    mcpwm_gpio_init(MCPWM_UNIT_0, MCPWM0A, (int)P14);
-    mcpwm_config_t pwmconfig;
-    pwmconfig.frequency = 100000;
-    pwmconfig.cmpr_a = 0;  // duty cycle of PWMxA = 0
-    pwmconfig.counter_mode = MCPWM_UP_COUNTER;
-    pwmconfig.duty_mode = MCPWM_DUTY_MODE_0;
-    mcpwm_init(MCPWM_UNIT_0, MCPWM_TIMER_0, &pwmconfig);
-    mcpwm_set_frequency(MCPWM_UNIT_0, MCPWM_TIMER_0, 10000);
-    printf("init\n");
-    while (1) {
-        adc2_get_raw(ADC2_CHANNEL_2, ADC_WIDTH_BIT_12, &result);
-        printf("%d\n", result);
-        vTaskDelay(1);
+    slp.write(1);
+    dir.write(1);
+    stp.write(0);
+    while(1){
+        stp.flip();
+        delay_ms(1);
     }
 }
