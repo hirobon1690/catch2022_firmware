07ba6bfc834a22718c8bc5e83ee568fa52fee5be
hirobon1690 <hirobon1690@gmail.com>
2022-08-05 17:40:11 +0900

    UPDATE: PMP(failed)

--- src/main.cpp
+++ src/main.cpp
@@ -19,6 +19,9 @@ void app_main(void);
 gpio dir(E05, OUTPUT);
 gpio slp(E04, OUTPUT);
 gpio stp(P18,OUTPUT);
+gpio pmp0(E06,OUTPUT);
+gpio pmp1(E01,OUTPUT);
+gpio vlv0(E02,OUTPUT);
 
 void delay_ms(int ms) {
     vTaskDelay(ms/portTICK_PERIOD_MS);
@@ -31,10 +34,15 @@ void app_main() {
     i2c.init();
     ex.set();
     slp.write(1);
-    dir.write(1);
+    dir.write(0);
     stp.write(0);
+    vlv0.write(1);
+    pmp0.write(1);
+    pmp1.write(1);
+    printf("OK\n");
     while(1){
-        stp.flip();
-        delay_ms(1);
+        pmp0.write(1);
+        pmp1.write(1);
+        delay_ms(100);
     }
 }
