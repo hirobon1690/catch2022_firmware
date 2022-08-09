07b0adc1613e5dccef5c3a895ee15ed007df20ba
hirobon1690 <hirobon1690@gmail.com>
2022-08-09 19:30:42 +0900

    UPDATE: ADC::Ok

--- src/main.cpp
+++ src/main.cpp
@@ -2,86 +2,43 @@
 #include <driver/gpio.h>
 #include <driver/i2c.h>
 #include <driver/mcpwm.h>
-#include <driver/uart.h>
 #include <esp32/rom/ets_sys.h>
 #include <rom/ets_sys.h>
 #include <stdio.h>
 #include "PinNames.h"
-#include "Ticker.h"
-#include "ads1015.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "gpio.h"
 #include "i2c.h"
 #include "pca9557.h"
-#include "servo.h"
-#include "stepper.h"
-#include "tof.h"
-#include "uart.h"
-#include <string.h>
+#include "ads1015.h"
 
 extern "C" {
 void app_main(void);
 }
 
-gpio dir(E05, OUTPUT);
-gpio slp(E04, OUTPUT);
-gpio stp(P18, OUTPUT);
-gpio pmp0(E06, OUTPUT);
-gpio pmp1(E01, OUTPUT);
-gpio vlv0(E02, OUTPUT);
-
-Ticker ticker;
-int stepCnt = 0;
-int stepCycle = 20;
-
-void step() {
-    if (stepCnt >= stepCycle) {
-        stepCnt = 0;
-    } else {
-        stepCnt += 50;
-    }
-    stp = (stepCycle != 0 && stepCnt < 100);
-}
+gpio dir0(E04, OUTPUT);
+gpio dir1(E01, OUTPUT);
 
 void delay_ms(int ms) {
-    vTaskDelay(ms / portTICK_RATE_MS);
+    vTaskDelay(ms/portTICK_RATE_MS);
 }
+
 
 
 void app_main() {
     delay_ms(10);
     i2c.init();
     ex.set();
+    adc pin0(A2);
+    int result=0;
 
-    printf("OKOK\n");
-    uart.init();
-
-    slp.write(1);
-    dir.write(0);
-    stp.write(0);
-    ticker.attach_us(50, step);
-    vlv0.write(1);
-    pmp0.write(1);
-    pmp1.write(1);
-    printf("OK\n");
-    servo servo(P19, 0, 0);
-    int pastangle=0;
+    printf("init\n");
     while (1) {
-        char sample[128];
-        printf("Enter Angle\n");
-        uart.read(sample);
-        int angle=atoi(sample);
-        printf("\nangle is %d\n",angle);
-        servo.duty(angle);
-        delay_ms(10);
-        // for(int angle=0;angle<=180;angle++){
-        //     servo.write(angle);
-        //     delay_ms(10);
-        // }
-        // for(int angle=180;angle>=0;angle--){
-        //     servo.write(angle);
-        //     delay_ms(10);
-        // }
+        result=pin0.read();
+        printf("%d\n",result);
+        delay_ms(100);    
+
+
     }
 }
