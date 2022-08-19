36b73065d8d5b0b6a6830e434a71ea50bedf75f5
hirobon1690 <hirobon1690@gmail.com>
2022-08-19 19:17:05 +0900

    CLR

--- src/main.cpp
+++ src/main.cpp
@@ -22,76 +22,10 @@ extern "C" {
 void app_main(void);
 }
 
-gpio dir(E05, OUTPUT);
-gpio slp(E04, OUTPUT);
-gpio stp(P18, OUTPUT);
-gpio pmp0(E06, OUTPUT);
-gpio pmp1(E01, OUTPUT);
-gpio vlv0(E02, OUTPUT);
-
-Ticker ticker;
-int stepCnt = 0;
-int stepCycle = 1200;
-
-char prevbuf[128];
-char buf[128];
-
-void IRAM_ATTR uartIsr(void *arg) {
-    int index = 0;
-    uart_read_bytes(UART_NUM_0, buf + index, 1, 10);
-    if (*(buf + index) == '\n') {
-        *(buf + index) = NULL;
-        memcpy(prevbuf,buf,sizeof(buf));
-        memset(buf, '\0', sizeof(buf));
-        return;
-    }
-    index++;
-}
-
-void step() {
-    if (stepCnt >= stepCycle) {
-        stepCnt = 0;
-    } else {
-        stepCnt += 50;
-    }
-    stp.write((stepCycle != 0 && stepCnt < 100));
-}
-
 void delay_ms(int ms) {
     vTaskDelay(ms / portTICK_RATE_MS);
 }
 
 void app_main() {
-    delay_ms(10);
-    i2c.init();
-    ex.set();
 
-    printf("OKOK\n");
-    uart.init();
-    uart.enableIsr(uartIsr);
-
-    slp.write(1);
-    dir.write(1);
-    stp.write(0);
-    ticker.attach_us(50, step);
-    vlv0.write(1);
-    pmp0.write(1);
-    pmp1.write(1);
-    printf("OK\n");
-    servo servo(P19, 0, 0);
-    int pastangle = 0;
-    while (1) {
-        // stp.flip();
-        delay_ms(10);
-        char sample[128];
-        printf("Enter Stepcycle\n");
-        uart.read(sample);
-        stepCycle = atoi(sample);
-        printf("\nCycle is %d\n", stepCycle);
-        // servo.duty(angle);
-        // delay_ms(10);
-    }
-
-    // while (1) {
-    // }
 }
