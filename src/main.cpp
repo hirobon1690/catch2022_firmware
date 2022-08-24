370b285168d40fb994bbb7abc8848006cf85e2e0
hirobon1690 <hirobon1690@gmail.com>
2022-08-24 16:01:38 +0900

    deleted

--- src/main.cpp
+++ src/main.cpp
@@ -3,22 +3,22 @@
 #include <driver/mcpwm.h>
 #include <driver/uart.h>
 #include <esp32/rom/ets_sys.h>
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
 #include <rom/ets_sys.h>
 #include <stdio.h>
 #include <string.h>
 #include "PinNames.h"
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
+#include "ads1015.h"
+#include "commonfunc.h"
 #include "gpio.h"
 #include "i2c.h"
 #include "mcpwm.h"
-#include "ads1015.h"
 #include "motor.h"
 #include "pca9557.h"
 #include "tof.h"
 #include "twai.h"
 #include "uart.h"
-#include "commonfunc.h"
 
 extern "C" {
 void app_main(void);
