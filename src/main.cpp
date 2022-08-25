074b942c656e2ecc253fd9e195a4914e8f2dd16a
hirobon1690 <hirobon1690@gmail.com>
2022-08-25 15:09:13 +0900

    UPDATE:

--- src/main.cpp
+++ src/main.cpp
@@ -35,19 +35,23 @@ int stepper_state;
 int pump_state;
 int emergency;
 
-motor m0(Pe1C,MCPWM_UNIT_0,MCPWM_TIMER_0,MCPWM0A,E04,E01);
-motor m1(Pe1D,MCPWM_UNIT_1,MCPWM_TIMER_1,MCPWM1A,E02,E03);
-gpio s00(Pe1A,INPUT_PU);
-gpio s01(Pe1B,INPUT_PU);
+const int pidPeriod=10;
+
+motor m0(Pe1A,MCPWM_UNIT_0,MCPWM_TIMER_0,MCPWM0A,E04,E01);
+motor m1(Pe1B,MCPWM_UNIT_1,MCPWM_TIMER_1,MCPWM1A,E02,E03);
+gpio s00(Pe1C,INPUT_PU);
+gpio s01(Pe1D,INPUT_PU);
 gpio s10(Pe2A,INPUT_PU);
 gpio s11(Pe2B,INPUT_PU);
 gpio user(USER,INPUT_PU);
+gpio pmp[2]={gpio(Pe2A,OUTPUT),gpio(Pe2B,OUTPUT)};
+gpio vlv[2]={gpio(Pe2C,OUTPUT),gpio(Pe2D,OUTPUT)};
 adc pot0(A2);
 adc pot1(A3);
 arm a0(m0,user,user,pot0,250);
 arm a1(m1,user,user,pot1,276);
-KRA_PID pid0(1,0,250,0,30);
-KRA_PID pid1(1,0,276,0,30);
+KRA_PID pid0(pidPeriod,0,250,0,30);
+KRA_PID pid1(pidPeriod,0,276,0,30);
 
 Ticker ticker0;
 Ticker ticker1;
@@ -60,27 +64,45 @@ int unpackInt(char* buf, int index) {
     return *(buf + index);
 }
 
+void turnPmp(bool val){
+    for(int i=0;i<2;i++){
+        pmp[i].write(val);
+        vlv[i].write(val);
+    }
+}
+
 void receiveUart(void* pvParameters) {
     while (1) {
         uart_read_bytes(UART_NUM_0, uart_msg, 16, portMAX_DELAY);
         if (uart_msg[15] != 0xFF) {
             printf("Connection Failed!\n");
+            while(1){
+                char buf[1];
+                uart_read_bytes(UART_NUM_0,buf,1,portMAX_DELAY);
+                if(buf[0]==0xFF){
+                    break;
+                }
+            }
         }
         move_cmd[0] = unpackFloat(uart_msg, 0);
         move_cmd[1] = unpackFloat(uart_msg, 4);
+        pid0.setgoal(move_cmd[0]);
+        pid1.setgoal(move_cmd[1]);
         servo_angle = unpackFloat(uart_msg, 8);
         stepper_state = unpackInt(uart_msg, 12);
-        pump_state = unpackInt(uart_msg, 13);
+        pump_state = (bool)unpackInt(uart_msg, 13);
+        turnPmp(pump_state);
         emergency = unpackInt(uart_msg, 14);
     }
 }
 
-void calA0PID(){
-    m0.write(pid0.calPID(a0.getDeg()));
-}
-
-void calA1PID(){
-    m1.write(pid1.calPID(a1.getDeg()));
+void calPID(){
+    float currentDeg[2]={0,0};
+    currentDeg[0]=a0.getDeg();
+    currentDeg[1]=a1.getDeg();
+    m0.write(pid0.calPID(currentDeg[0]));
+    m1.write(pid1.calPID(currentDeg[1]));
+    printf("%f, %f\n",currentDeg[0],currentDeg[1]);
 }
 
 void app_main() {
@@ -89,10 +111,12 @@ void app_main() {
     uart.init();
     twai.init();
     pid0.setgain(10,0,0);
-    pid1.setgain(10,1,1);
-    ticker0.attach_ms(1,calA0PID);
-    ticker1.attach_ms(1,calA1PID);
+    pid1.setgain(10,0,0);
     a0.home(30);
+    pid0.setgoal(125);
+
+    ticker0.attach_ms(pidPeriod,calPID);
+    // ticker1.attach_ms(pidPeriod,calA1PID);
     xTaskCreatePinnedToCore(receiveUart, "receiveUart", 4096, NULL, 22, &taskHandle, 0);
     while (1) {
         delay_ms(10);
