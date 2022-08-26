5a08b21901d452405aa5d099176d65c0a378e679
hirobon1690 <hirobon1690@gmail.com>
2022-08-26 12:43:04 +0900

    a

--- src/main.cpp
+++ src/main.cpp
@@ -35,7 +35,7 @@ int stepper_state;
 int pump_state;
 int emergency;
 
-const int pidPeriod = 20;
+const int pidPeriod = 10;
 
 motor m0(Pe1A, MCPWM_UNIT_0, MCPWM_TIMER_0, MCPWM0A, E04, E01);
 motor m1(Pe1B, MCPWM_UNIT_1, MCPWM_TIMER_1, MCPWM1A, E03, E02);
@@ -48,9 +48,9 @@ gpio pmp[2] = {gpio(Pe0A, OUTPUT), gpio(Pe0B, OUTPUT)};
 gpio vlv[2] = {gpio(Pe0C, OUTPUT), gpio(Pe0D, OUTPUT)};
 adc pot0(A2);
 adc pot1(A3);
-arm a0(m0, user, user, pot0, 250);
-arm a1(m1, user, user, pot1, 276);
-KRA_PID pid0(pidPeriod, 0, 250, 0, 30);
+arm a0(m0, s00, s01, pot0, 250);
+arm a1(m1, s10, s11, pot1, 276);
+KRA_PID pid0(pidPeriod, 0, 250, 0, 15);
 KRA_PID pid1(pidPeriod, 0, 276, 0, 30);
 
 Ticker ticker0;
@@ -96,17 +96,30 @@ void receiveUart(void* pvParameters) {
     }
 }
 
-void calPID(void* pvParameters) {
+void calPID() {
     float currentDeg[2] = {0, 0};
-    while (1) {
-        currentDeg[0] = pot0.read();
-        delay_ms(10);
-        currentDeg[1] = pot1.read();
-        delay_ms(10);
+    // while (1) {
+        currentDeg[0] = a0.getDeg();
+        // delay_ms(2);
+        currentDeg[1] = a1.getDeg();
+        // delay_ms(2);
         // m0.write(pid0.calPID(currentDeg[0]));
         // m1.write(pid1.calPID(currentDeg[1]));
         printf("%f, %f\n", currentDeg[0], currentDeg[1]);
-        delay_ms(pidPeriod - 2);
+        // delay_ms(8);
+    // }
+}
+
+void adctest(void* pvParameters){
+    float currentDeg[2] = {0, 0};
+    while (1) {
+        currentDeg[0] = a0.getDeg();
+        delay_ms(3);
+        currentDeg[1] = a1.getDeg();
+        delay_ms(3);
+        m0.write(pid0.calPID(currentDeg[0]));
+        // m1.write(pid1.calPID(currentDeg[1]));
+        printf("%3.2f, %3.2f\n", currentDeg[0], currentDeg[1]);
     }
 }
 
@@ -117,14 +130,14 @@ void app_main() {
     twai.init();
     pid0.setgain(10, 0, 0);
     pid1.setgain(10, 0, 0);
-    // a0.home(15);
+    a0.home(15);
     // a1.home(30);
     pid0.setgoal(125);
     // ticker0.attach_ms(pidPeriod, calPID);
 
     // ticker1.attach_ms(pidPeriod,calA1PID);
     xTaskCreatePinnedToCore(receiveUart, "receiveUart", 4096, NULL, 22, &taskHandle, 0);
-    xTaskCreatePinnedToCore(calPID, "calPID", 4096, NULL, 22, &taskHandle, 1);
+    xTaskCreatePinnedToCore(adctest, "adctest", 4096, NULL, 22, &taskHandle, 1);
     while (1) {
         delay_ms(10);
     }
