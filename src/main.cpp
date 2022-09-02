b1dcc2644e0813592dd5db57fba91a9443573cb7
hirobon1690 <hirobon1690@gmail.com>
2022-09-02 12:38:35 +0900

    FIX: gpio12, 13 OK

--- src/main.cpp
+++ src/main.cpp
@@ -1,134 +1,31 @@
 #include <driver/gpio.h>
 #include <driver/i2c.h>
-#include <driver/mcpwm.h>
-#include <driver/uart.h>
 #include <esp32/rom/ets_sys.h>
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
 #include <rom/ets_sys.h>
 #include <stdio.h>
-#include <string.h>
-#include "PinNames.h"
-#include "ads1015.h"
 #include "commonfunc.h"
-#include "gpio.h"
-#include "i2c.h"
-#include "mcpwm.h"
-#include "motor.h"
-#include "pca9557.h"
-#include "tof.h"
-#include "twai.h"
-#include "uart.h"
+
 
 extern "C" {
 void app_main(void);
 }
 
-gpio dir(E05, OUTPUT);
-gpio slp(E04, OUTPUT);
-gpio stp(P18, OUTPUT);
-gpio pmp0(Pe1A, OUTPUT);
-gpio pmp1(Pe1B, OUTPUT);
-gpio vlv0(Pe1C, OUTPUT);
-gpio vlv1(Pe1D,OUTPUT);
-
-// Ticker ticker;
-int stepCnt = 0;
-int stepCycle = 0;
-
-char prevbuf[128];
-char buf[128];
-
-// void IRAM_ATTR uartIsr(void *arg) {
-//     int index = 0;
-//     uart_read_bytes(UART_NUM_0, buf + index, 1, 10);
-//     if (*(buf + index) == '\n') {
-//         *(buf + index) = NULL;
-//         memcpy(prevbuf,buf,sizeof(buf));
-//         memset(buf, '\0', sizeof(buf));
-//         return;
-//     }
-//     index++;
-// }
-
-void step() {
-    if (stepCnt >= stepCycle) {
-        stepCnt = 0;
-    } else {
-        stepCnt += 50;
-    }
-    stp.write((stepCycle != 0 && stepCnt < 100));
-    if(stepCycle==0){
-        slp.write(1);
-    }else{
-        slp.write(0);
-    }
-}
-
-void delay_ms(int ms) {
-    vTaskDelay(ms / portTICK_RATE_MS);
-}
-
 void app_main() {
-    delay_ms(10);
-    i2c.init();
-    ex.set();
-
-    printf("OKOK\n");
-    uart.init();
-    // uart.enableIsr(uartIsr);
-
-    // slp.write(1);
-    // dir.write(1);
-    // stp.write(0);
-    // ticker.attach_us(50, step);
-    // vlv0.write(1);
-    // pmp0.write(1);
-    // pmp1.write(1);
-    printf("OK\n");
-    // servo servo(P19, 0, 0);
-    // int pastangle = 0;
-    // printf("Enter Dir\n");
-    // char direction[128];
-    // uart.read(direction);
-    // dir.write((bool)atoi(direction));
-    
-    while (1) {
-        // stp.flip();
-        delay_ms(10);
-        char sample[128];
-        printf("Enter Pomp State\n");
-        uart.read(sample);
-        switch(atoi(sample)){
-            case 0:
-                pmp0.write(1);
-                vlv0.write(0);
-                break;
-            case 1:
-                pmp0.write(1);
-                vlv0.write(1);
-                break;
-            case 2:
-                pmp1.write(1);
-                vlv1.write(0);
-                break;
-            case 3:
-                pmp1.write(1);
-                vlv1.write(1);
-                break;
-            case 4:
-                vlv0.write(0);
-                vlv1.write(0);
-                break;
-            case 5:
-                vlv0.write(1);
-                vlv1.write(1);
-                break;
-
-        }
-        // stepCycle = atoi(sample);
-        // printf("\nCycle is %d\n", stepCycle);
-        // servo.duty(angle);
-        // delay_ms(10);
+    gpio_config_t conf = {
+        .pin_bit_mask = (0b11ULL<<12),                 /*!< GPIO pin: set with bit mask, each bit maps to a GPIO */
+        .mode = GPIO_MODE_DISABLE,                   /*!< GPIO mode: set input/output mode                     */
+        .pull_up_en = GPIO_PULLUP_DISABLE,           /*!< GPIO pull-up                                         */
+        .pull_down_en = GPIO_PULLDOWN_DISABLE,       /*!< GPIO pull-down                                       */
+        // .intr_type = gpiohal.dev->pin[pin].int_type  /*!< GPIO interrupt type - previously set                 */
+    };
+    gpio_config(&conf);
+    gpio_set_direction(GPIO_NUM_13,GPIO_MODE_OUTPUT);
+    gpio_set_level(GPIO_NUM_13,1);
+    gpio_set_level(GPIO_NUM_12,1);
+    while(1){
+        delay_ms(100);
     }
+
 }
