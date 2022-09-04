28e7e64c2108c7b36663db199bc55b9547f8c23b
hirobon1690 <hirobon1690@gmail.com>
2022-09-04 16:25:04 +0900

    ADD: library

--- src/main.cpp
+++ src/main.cpp
@@ -18,7 +18,7 @@
 #include "stepper.h"
 #include "tof.h"
 #include "uart.h"
-#include "vl53l0x.h"
+
 
 extern "C" {
 void app_main(void);
@@ -30,32 +30,41 @@ void delay_ms(int ms) {
 
 int rawData[2] = {0, 0};
 
+
 void app_main() {
     i2c.init();
     ex.set();
-    gpio pin0(Pe2A, OUTPUT);
-    gpio pin1(Pe2B, OUTPUT);
-    gpio pin2(Pe2C, OUTPUT);
-    gpio pin3(Pe2D, OUTPUT);
-    tof tof1(pin0);
-    pin0.write(0);
-    pin1.write(0);
-    pin2.write(0);
-    pin3.write(0);
-    tof tof0(pin0);
+
+    // gpio pin0(Pe2A, OUTPUT);
+    // gpio pin1(Pe2B, OUTPUT);
+    // gpio pin2(Pe2C, OUTPUT);
+    // gpio pin3(Pe2D, OUTPUT);
+    // tof tof1(pin0);
+    // pin0.write(0);
+    // pin1.write(0);
+    // pin2.write(0);
+    // pin3.write(0);
+    // tof tof0(pin0);
     // tof tof1(pin2);
     // tof tof2(Pe2D);
-    
+    // vl53l0x_setTimeout(sensor, (uint16_t)500);
+    // if (vl53l0x_init(sensor)) {
+    //     printf("Failed to detect and initialize sensor!");
+    //     while (1) {
+    //     }
+    // }
+    int dist=0;
     while (1) {
         // pin0.flip();
         // pin1.flip();
         // pin2.flip();
         // pin3.flip();
         // delay_ms(500);
-        pin0.write(1);
-        int dist;
-        dist=tof0.read();
-        printf("%d\n",dist);
+        // pin0.write(1);
+        // int dist;
+        // dist=tof0.read();
+
+        printf("%d\n", dist);
         delay_ms(100);
     }
 }
