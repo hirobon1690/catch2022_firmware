d947e4e26cd89488428ff7a616d9372bebc7cecd
hirobon1690 <hirobon1690@gmail.com>
2022-09-04 19:59:35 +0900

    UPDATE: ToF::OK!!!

--- src/main.cpp
+++ src/main.cpp
@@ -21,14 +21,8 @@
 #include "uart.h"
 
 #include "VL53L0X.h"
-
-#include "esp_log.h"
-#define TAG "app_main"
-
-/* config */
-#define I2C_PORT I2C_NUM_0
-#define PIN_SDA GPIO_NUM_21
-#define PIN_SCL GPIO_NUM_22
+VL53L0X sensor;
+VL53L0X sensor1;
 
 extern "C" {
 void app_main(void);
@@ -39,28 +33,16 @@ void app_main() {
     /* initialization */
     i2c.init();
     pin0.write(1);
+    sensor.init();
+    sensor.setAddress(0x30);
     pin1.write(1);
-    VL53L0X vl(I2C_PORT, (gpio_num_t)Pe2C);
-    VL53L0X vl1(I2C_PORT, (gpio_num_t)Pe2A);
-    vl.setDeviceAddress(0x30);
-    // vl1.setDeviceAddress(0x29);
-
-    if (!vl.init()) {
-        ESP_LOGE(TAG, "Failed to initialize VL53L0X :(");
-        vTaskDelay(portMAX_DELAY);
-    }
-
+    sensor1.init();
     while (1) {
-        /* measurement */
-        uint16_t result_mm = 0;
-        TickType_t tick_start = xTaskGetTickCount();
-        bool res = vl.read(&result_mm);
-        TickType_t tick_end = xTaskGetTickCount();
-        int took_ms = ((int)tick_end - tick_start) / portTICK_PERIOD_MS;
-        if (res)
-            ESP_LOGI(TAG, "Range: %d [mm] took %d [ms]", (int)result_mm, took_ms);
-        else
-            ESP_LOGE(TAG, "Failed to measure :(");
+        int dist[2];
+        dist[0] = sensor.readRangeSingleMillimeters();
+        dist[1] = sensor1.readRangeSingleMillimeters();
+        printf("%d, %d\n", dist[0],dist[1]);
+        delay_ms(100);
     }
 }
 
